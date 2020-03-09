d50bab9914f516d572e2984a0f500daf4aa064e1
p2444354 <p2444354@GWHXX473WIN012W>
2020-03-09 14:14:27 +0000

    Created Test For Game Controller

---
+++ MVC/MVC.Tests/Controllers/GameController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MVC.Tests.Controllers
+{
+    [TestClass]
+    public class GameController
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
