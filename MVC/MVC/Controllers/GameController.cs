51682e5980fc0f0a4660912df077ae3be29e23d2
p2444354 <p2444354@GWHXX473WIN012W>
2020-03-09 15:15:31 +0000

    Edited GameController.AnGame action method to return the model

--- MVC/MVC/Controllers/GameController.cs
+++ MVC/MVC/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,14 +22,14 @@ namespace MVC.Controllers
         //method for an game
         //this is a simple example of an action method
         //that acceps some data from a HTTP GET request
-        public string AnGame(int Id, string Genre, string Platform,  string Price)
+        public ViewResult AnGame()
         {
-            //return "Game: Id = " + id.ToString();
+            AnGameModel GameModel = new AnGameModel();
+            GameModel.Id = 1;
+            GameModel.Genre = "Action";
+            GameModel.Platform = "PC";
+            GameModel.Price = "£12";
 
-            //To try out the code below, use his url
-            //localhost:3552/Game/AnGame?Id=1&Genre=Action&Platform=Pc&Price=12
-
-            return String.Format("Game ID = {0}, Genre={1}, Platform={2}, Price={3}", Id, Genre, Platform, Price);
-        }
+            return View(GameModel);
     }
 }
