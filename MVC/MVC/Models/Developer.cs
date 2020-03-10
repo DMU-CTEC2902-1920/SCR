91e6bb86551d1a2e82b428e1b1366f64c8d7c66c
p2444354 <p2444354@GWHXX473WIN012W>
2020-03-10 15:54:41 +0000

    Added some data valiadtion to developer

--- MVC/MVC/Models/Developer.cs
+++ MVC/MVC/Models/Developer.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC.Models
 {
     public class Developer
     {
         public int DeveloperId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [StringLength(100)]
         public string Description { get; set; }
         public string Platform { get; set; }
+        [Range(typeof(decimal), "1", "10")]
         public decimal Rating{ get; set; }
     }
 }
