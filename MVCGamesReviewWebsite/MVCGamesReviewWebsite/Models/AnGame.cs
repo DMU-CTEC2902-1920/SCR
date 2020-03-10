15b6e5787a3be3adf5d687903994ed47084da33d
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-03-10 19:25:40 +0000

    Added attributes for AnGame

--- MVCGamesReviewWebsite/MVCGamesReviewWebsite/Models/AnGame.cs
+++ MVCGamesReviewWebsite/MVCGamesReviewWebsite/Models/AnGame.cs
@@ -7,7 +7,13 @@ namespace MVCGamesReviewWebsite.Models
 {
     public class AnGame
     {
-        
+        public virtual int GameId { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+
+        public virtual Genre Genre { get; set; }
+        public virtual string Platform { get; set; }
 
     }
 }
