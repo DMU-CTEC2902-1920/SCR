4eb6c0f3e7e4be7247ff5c6a1b8970fdd770454d
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-03-10 00:06:51 +0000

    Added genre class

---
+++ MVC/MVC/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Genre
+    {
+        //declared virtual so can be overidden
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
