c3f6fe70bc6f878ece5191fc0605e384521b95cd
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-03-10 19:15:16 +0000

    Added One more attribute to An Game, and constructed a data initialiser

---
+++ MVCGameReviewWebsite/MVCGameReviewWebsite/Models/AnGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGameReviewWebsite.Models
+{
+    public class AnGame
+    {
+        public virtual int Id { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Platform { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual decimal Price { get; set; }
+
+
+    }
+}
