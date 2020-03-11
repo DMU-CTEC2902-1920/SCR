77b36b592cf423f1b2746a0f2cb191685f73ceed
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-03-11 01:39:59 +0000

    Added DeveloperAnGameViewModel

---
+++ MVCGamesReviewWebsite/MVCGamesReviewWebsite/Models/DeveloperAnGameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGamesReviewWebsite.Models
+{
+    public class DeveloperAnGameViewModel
+    {
+        public AnGame AnGame { get; set; }
+        public Developer Developer { get; set; }
+    }
+}
