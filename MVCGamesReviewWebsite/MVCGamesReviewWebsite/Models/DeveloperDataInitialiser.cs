243f0d73fd59644b58506a95e522c581a15d7935
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-03-11 00:04:34 +0000

    Completed developer initialiser

---
+++ MVCGamesReviewWebsite/MVCGamesReviewWebsite/Models/DeveloperDataInitialiser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MVCGamesReviewWebsite.Models
+{
+    public class DeveloperDataInitialiser : DropCreateDatabaseAlways<DeveloperContext>
+
+    {
+        protected override void Seed(DeveloperContext context)
+        {
+
+            Developer del1 = new Developer();
+            del1.Id = 1;
+            del1.Name = "George";
+            del1.Description = "Great Developer";
+            Developer del2 = new Developer();
+            del1.Id = 2;
+            del1.Name = "Micheal";
+            del1.Description = "Good  Developer";
+            base.Seed(context);
+
+    }   }
+        
+    
+}
