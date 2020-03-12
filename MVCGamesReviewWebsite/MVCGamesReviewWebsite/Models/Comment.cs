9d434d6854d571a94df54cd356c31b0f7c34c318
Callum Simpson <P2444354@my365.dmu.ac.uk>
2020-03-12 01:23:40 +0000

    Added three new models so i can start  the commenting section of the project. Variables declared in Comment and user

---
+++ MVCGamesReviewWebsite/MVCGamesReviewWebsite/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGamesReviewWebsite.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+
+        public int UserId { get; set; }
+        public String Text { get; set; }
+
+        
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ MVCGamesReviewWebsite/MVCGamesReviewWebsite/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCGamesReviewWebsite.Models
+{
+    public class User
+    {
+        public virtual int UserId { get; set; }
+
+        public virtual string Password { get; set; }
+
+        public virtual string Email { get; set; }
+    }
+}
